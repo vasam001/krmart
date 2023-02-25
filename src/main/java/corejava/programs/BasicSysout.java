57cc38d2135c238f3f33e7d918166c987ece43f7
kiranreddym76 <kiranreddym76@gmail.com>
2023-02-25 12:44:24 +0530

    basic java program

---
+++ src/main/java/corejava/programs/BasicSysout.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package corejava.programs;
+
+/**
+ * @author kiran
+ *
+ */
+public class BasicSysout {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("basic program");
+	}
+
+}
