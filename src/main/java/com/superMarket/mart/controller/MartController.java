019688f0d0b21fcfa3bc9468a41ee07030622d70
kiranreddym76 <kiranreddym76@gmail.com>
2023-02-25 10:19:17 +0530

    change mart name

--- src/main/java/com/superMarket/mart/controller/MartController.java
+++ src/main/java/com/superMarket/mart/controller/MartController.java
@@ -43,7 +43,7 @@ public class MartController {
 
 	@GetMapping("/welcome")
 	public String welcome(@RequestParam String name) {
-		String message = "welocme to Mart " + name;
+		String message = "welocme to Kr Mart  " + name;
 		return message;
 	}
 
